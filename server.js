ab3c0a24bb4a3188bd3bc9453ffd8a07432c3b1e
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-07-18 11:42:39 +0530

    feat: Implement user authentication with registration and login routes
    .env add the token my own

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 connectDB(); // connect to MongoDB
@@ -11,9 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
