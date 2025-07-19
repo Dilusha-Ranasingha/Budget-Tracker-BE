f90264dcd5a22745088b14f3306a50fb8a6b4a68
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-07-19 23:27:04 +0530

    feat: Add Transaction model with schema for user transactions

---
+++ models/Transaction.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const transactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  type: {
+    type: String,
+    enum: ['income', 'expense'],
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  remark: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+export default Transaction;
